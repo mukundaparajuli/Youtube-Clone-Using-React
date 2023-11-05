34372d23c3363066e7393705961563ed265ccfb9
Mukunda Parajuli <mukundaparajuli13@gmail.com>
2023-11-05 07:30:59 +0545

    handled the dependency error

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,5 +1,5 @@
 const GOOGLE_API_KEY = "AIzaSyB-Q4AHucljMa_U1ItJ0h5FI9zUR9JT2Uw";
-export const OFFSET_LIVE_CHAT = 18;
+export const OFFSET_LIVE_CHAT = 14;
 export const YOUTUBE_VIDEOS_APIS =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=NP&key=" +
   GOOGLE_API_KEY;
